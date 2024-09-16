e6dbbdda4bc63beae60709b2396ac6b94bbe1505
FishM8 <136879354+FishM8WasHere@users.noreply.github.com>
2024-09-16 20:26:01 +0700

    README.py

---
+++ README.py
@@ -0,0 +1,22 @@
+# so first we make a list of our list, then make a list inside of a list like: [[], [], []]
+# repeat the .append to the list by range(y) first then range(x)
+
+# lets say the empty square value is [' ']
+# and our player spawn with the ['A'] square :)
+
+# sorry i kept disconecting my flashdisk while typing ....
+
+# then we assign our player x, y to our current map that shows in the output
+# do a loop, then if the player moves (using input (w,a,s,d) then enter), we assign the old position
+# then delete from the current map by a for loop, and .append our A square/player square to the
+# new square in the map on our output :) 
+
+# also i kept always do some typo
+
+# lets run it!
+# uhh, what?
+
+# protip: dont ever use random int.... :V
+
+# ill fix some bugs but thats all cya in the later videos :)
+# code will be on my github :)
